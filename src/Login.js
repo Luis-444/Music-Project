712e7922d5278e9cfe0d53874f5c9070e32b957d
Luis-444 <luismanuelmaganaloyo@gmail.com>
2021-05-03 23:05:30 -0500

    Base de proyecto

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css';
+
+function Login(){
+    return (
+        <div className="login">
+            <img src="https://images.pexels.com/photos/145707/pexels-photo-145707.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="imagen"/>
+            <a>LOGIN WITH SPOTIFY</a>
+        </div>
+    );
+}
+
+export default Login;
